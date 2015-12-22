dc862dc6c54fb74c718883a37f3eba9fcc4608fb
Tim Butler <tim.butler@harcourts.net>
2015-12-22 11:57:44 +1000

    Upgraded Grav to v1.0.5 and plugins: 01. Admin Panel     [v1.0.0-rc.7 ➜ v1.0.3] 02. Email           [v1.0.0 ➜ v2.1.0] 03. Error           [v1.4.0 ➜ v1.4.1] 04. Form            [v1.0.2 ➜ v1.1.0] 05. Login           [v1.1.0 ➜ v1.2.1] 06. Problems        [v1.2.0 ➜ v1.3.2] 07. Deliver         [v1.3.0 ➜ v1.3.1]

--- system/src/Grav/Common/Markdown/ParsedownGravTrait.php
+++ system/src/Grav/Common/Markdown/ParsedownGravTrait.php
@@ -3,6 +3,7 @@ namespace Grav\Common\Markdown;
 
 use Grav\Common\GravTrait;
 use Grav\Common\Uri;
+use RocketTheme\Toolbox\Event\Event;
 
 /**
  * A trait to add some custom processing to the identifyLink() method in Parsedown and ParsedownExtra
@@ -15,8 +16,10 @@ trait ParsedownGravTrait
     protected $base_url;
     protected $pages_dir;
     protected $special_chars;
-
     protected $twig_link_regex = '/\!*\[(?:.*)\]\((\{([\{%#])\s*(.*?)\s*(?:\2|\})\})\)/';
+
+    public $completeable_blocks = [];
+    public $continueable_blocks = [];
 
     /**
      * Initialization function to setup key variables needed by the MarkdownGravLinkTrait
@@ -26,8 +29,10 @@ trait ParsedownGravTrait
      */
     protected function init($page, $defaults)
     {
+        $grav = self::getGrav();
+
         $this->page = $page;
-        $this->pages = self::getGrav()['pages'];
+        $this->pages = $grav['pages'];
         $this->BlockTypes['{'] [] = "TwigTag";
         $this->base_url = rtrim(self::getGrav()['base_url'] . self::getGrav()['pages']->base(), '/');
         $this->pages_dir = self::getGrav()['locator']->findResource('page://');
@@ -41,7 +46,66 @@ trait ParsedownGravTrait
         $this->setUrlsLinked($defaults['auto_url_links']);
         $this->setMarkupEscaped($defaults['escape_markup']);
         $this->setSpecialChars($defaults['special_chars']);
+
+        $grav->fireEvent('onMarkdownInitialized', new Event(['markdown' => $this]));
+
     }
+
+    /**
+     * Be able to define a new Block type or override an existing one
+     *
+     * @param $type
+     * @param $tag
+     */
+    public function addBlockType($type, $tag, $continueable = false, $completeable = false)
+    {
+        $this->BlockTypes[$type] []= $tag;
+
+        if ($continueable) {
+            $this->continueable_blocks[] = $tag;
+        }
+
+        if ($completeable) {
+            $this->completeable_blocks[] = $tag;
+        }
+    }
+
+    /**
+     * Be able to define a new Inline type or override an existing one
+     *
+     * @param $type
+     * @param $tag
+     */
+    public function addInlineType($type, $tag)
+    {
+        $this->InlineTypes[$type] []= $tag;
+        $this->inlineMarkerList .= $type;
+    }
+
+    /**
+     * Overrides the default behavior to allow for plugin-provided blocks to be continueable
+     *
+     * @param $Type
+     * @return bool
+     */
+    protected function isBlockContinueable($Type)
+    {
+        $continueable = in_array($Type, $this->continueable_blocks) || method_exists($this, 'block'.$Type.'Continue');
+        return $continueable;
+    }
+
+    /**
+     *  Overrides the default behavior to allow for plugin-provided blocks to be completeable
+     *
+     * @param $Type
+     * @return bool
+     */
+    protected function isBlockCompleteable($Type)
+    {
+        $completeable = in_array($Type, $this->completeable_blocks) || method_exists($this, 'block'.$Type.'Complete');
+        return $completeable;
+    }
+
 
     /**
      * Make the element function publicly accessible, Medium uses this to render from Twig
@@ -73,7 +137,7 @@ trait ParsedownGravTrait
      */
     protected function blockTwigTag($Line)
     {
-        if (preg_match('/[{%|{{|{#].*[#}|}}|%}]/', $Line['body'], $matches)) {
+        if (preg_match('/(?:{{|{%|{#)(.*)(?:}}|%}|#})/', $Line['body'], $matches)) {
             $Block = array(
                 'markup' => $Line['body'],
             );
@@ -204,6 +268,48 @@ trait ParsedownGravTrait
         if (isset($excerpt['element']['attributes']['href'])) {
             $url = parse_url(htmlspecialchars_decode($excerpt['element']['attributes']['href']));
 
+            // if there is a query, then parse it and build action calls
+            if (isset($url['query'])) {
+                $actions = array_reduce(explode('&', $url['query']), function ($carry, $item) {
+                    $parts = explode('=', $item, 2);
+                    $value = isset($parts[1]) ? $parts[1] : null;
+                    $carry[$parts[0]] = $value;
+
+                    return $carry;
+                }, []);
+
+                // valid attributes supported
+                $valid_attributes = ['rel', 'target', 'id', 'class', 'classes'];
+
+                // Unless told to not process, go through actions
+                if (array_key_exists('noprocess', $actions)) {
+                    unset($actions['noprocess']);
+                } else {
+                    // loop through actions for the image and call them
+                    foreach ($actions as $attrib => $value) {
+                        $key = $attrib;
+
+                        if (in_array($attrib, $valid_attributes)) {
+                            // support both class and classes
+                            if ($attrib == 'classes') {
+                                $attrib = 'class';
+                            }
+                            $excerpt['element']['attributes'][$attrib] = str_replace(',', ' ', $value);
+                            unset($actions[$key]);
+                        }
+                    }
+                }
+
+
+                $url['query']= http_build_query($actions, null, '&', PHP_QUERY_RFC3986);
+            }
+
+            // if no query elements left, unset query
+            if (empty($url['query'])) {
+                unset ($url['query']);
+            }
+
+
             // if there is no scheme, the file is local
             if (!isset($url['scheme']) && (count($url) > 0)) {
                 // convert the URl is required
@@ -213,4 +319,16 @@ trait ParsedownGravTrait
 
         return $excerpt;
     }
+
+    // For extending this class via plugins
+    public function __call($method, $args)
+    {
+        if (isset($this->$method) === true) {
+            $func = $this->$method;
+            return call_user_func_array($func, $args);
+        }
+    }
+
+
+
 }
